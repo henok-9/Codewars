ad3700aee55454e20d06d3883b5c28c23ee028ad
henok-9 <henokabe89@gmail.com>
2021-03-09 22:20:56 -0500

    Update credit_card_mask.py

--- credit_card_mask.py
+++ credit_card_mask.py
@@ -11,7 +11,7 @@ maskify(     "64607935616") ==      "#######5616"
 maskify(               "1") ==                "1"
 maskify(                "") ==                 ""
 
-# "What was the name of your first pet?"
+"What was the name of your first pet?"
 maskify("Skippy")                                   == "##ippy"
 maskify("Nananananananananananananananana Batman!") == "####################################man!" """
 
